03899a7ab69e16e746db5b08808749947220ac07
Tomasz Zielinski <Tomasz.Zielinski@vimn.com>
2021-11-07 20:32:02 +0100

    exter srectern

--- src/screen.cpp
+++ src/screen.cpp
@@ -4,57 +4,9 @@
 namespace screen
 {
 
-    IconsIndicatorsType iconsIndicator{0,0,0};
-   
+    IconsIndicatorsType iconsIndicator{0, 0, 0};
+
     Adafruit_PCD8544 display = Adafruit_PCD8544(11, 10, 9, 8);
-
-    constexpr byte fish[] PROGMEM = {
-		B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B01111111,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000011,B10000000,B11100000,B00000000,B00000000,
-		B00000000,B00000000,B00001101,B10000000,B00011000,B00000000,B00000000,
-		B00000000,B00000000,B00110001,B10000000,B00000110,B00000000,B00000000,
-		B00000000,B00000000,B01100101,B00000000,B00000011,B00000000,B00000000,
-		B00000111,B00000000,B11001001,B00000000,B00000001,B10000000,B00000000,
-		B00001000,B10000001,B00001001,B00000000,B00000000,B11000000,B00000000,
-		B00010000,B11000011,B00000001,B00000000,B01111000,B01100000,B00000000,
-		B00010000,B01000010,B00000001,B00000000,B11011100,B00100000,B00000000,
-		B00010000,B00100100,B00001001,B00000000,B11111100,B00110000,B00000000,
-		B00010000,B00100100,B10001001,B00000000,B11111100,B00010000,B00000000,
-		B00010000,B00101100,B10000101,B00000000,B11111100,B00011000,B00000000,
-		B00010000,B00111000,B10000001,B00000000,B01111000,B00001000,B00000000,
-		B00011000,B00011000,B10000001,B00000000,B00000000,B00001000,B00000000,
-		B00001000,B00011000,B10000001,B10000000,B00000000,B00001000,B00000000,
-		B00000100,B00011000,B01000100,B10000000,B00000000,B00001000,B00000000,
-		B00000011,B00011010,B00100100,B10000000,B00000000,B00001000,B00000000,
-		B00000011,B00011000,B00010100,B10000000,B00000010,B00001000,B00000000,
-		B00000100,B00011000,B00000100,B11000000,B00000000,B11001000,B00000000,
-		B00001000,B00011010,B00010100,B01000000,B00000000,B00001000,B00000000,
-		B00011000,B00011010,B00010010,B01100000,B00000000,B00001000,B00000000,
-		B00010000,B00111001,B00010001,B00100000,B00000000,B00001000,B00000000,
-		B00010000,B00101101,B10010000,B10110000,B00000000,B00011000,B00000000,
-		B00010000,B00100100,B10010000,B00010000,B00000000,B00010000,B00000000,
-		B00010000,B01100100,B00010000,B00011000,B00000000,B00110000,B00000000,
-		B00010000,B01000010,B00001000,B01001100,B00000000,B00100000,B00000000,
-		B00011000,B11000011,B00001100,B01000110,B00000000,B01100000,B00000000,
-		B00001101,B10000001,B10000000,B01000010,B00000000,B11000000,B00000000,
-		B00000111,B00000000,B10000000,B00100001,B00000001,B10000000,B00000000,
-		B00000000,B00000000,B01100000,B00010000,B11000011,B00000000,B00000000,
-		B00000000,B00000000,B00110000,B00001100,B01100110,B00000000,B00000000,
-		B00000000,B00000000,B00001100,B00000000,B00111000,B00000000,B00000000,
-		B00000000,B00000000,B00000011,B11000000,B11100000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B01111111,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-		B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-		};
-
-    constexpr byte refresh[] PROGMEM= {
-        B00111110,B00000000,B01100011,B00000000,B11000001,B10000000,B10000000,B10000000,
-        B10000010,B10100000,B11000001,B11000000,B01100000,B10000000,B00110000,B00000000,
-        };
 
     void initializeScreen()
     {
@@ -71,76 +23,91 @@ namespace screen
 
     void showLogo()
     {
-        display.drawBitmap(14, 4, fish, 56, 40, BLACK);        
+        display.drawBitmap(14, 4, fish, 56, 40, BLACK);
         display.display();
         delay(5000);
-        clearScreen();   
+        clearScreen();
     }
 
-    void showReadings(uint16_t bottomBarLenght, const char* name, UF16x2 value, bool showRefreshIcon)
+    void showReadings(uint16_t bottomBarLenght, const char *name, UF16x2 value, bool showRefreshIcon)
     {
         display.clearDisplay();
         display.setTextColor(BLACK);
         display.setTextSize(1);
-        
-        if (iconsIndicator.refill){display.setCursor(40,0); display.print("D");};
-        if (iconsIndicator.heater){display.setCursor(50,0); display.print("G");};
-        if (iconsIndicator.hci){display.setCursor(60,0); display.print("K");};
 
-        display.setCursor(0,0);
-        display.print(name); display.println(':');
-    
-        
+        if (iconsIndicator.refill)
+        {
+            display.setCursor(40, 0);
+            display.print("D");
+        };
+        if (iconsIndicator.heater)
+        {
+            display.setCursor(50, 0);
+            display.print("G");
+        };
+        if (iconsIndicator.hci)
+        {
+            display.setCursor(60, 0);
+            display.print("K");
+        };
+
+        display.setCursor(0, 0);
+        display.print(name);
+        display.println(':');
+
         display.println();
         display.setTextSize(3);
-        if (value >=10) display.println(value.asFloat(),1);
-        else display.println(value.asFloat(),2);
+        if (value >= 10)
+            display.println(value.asFloat(), 1);
+        else
+            display.println(value.asFloat(), 2);
 
         if (showRefreshIcon)
-	  	{
+        {
 
-	        display.drawBitmap(73,6,refresh,16,8, BLACK);
+            display.drawBitmap(73, 6, refresh, 16, 8, BLACK);
 
             display.setTextSize(1);
             for (uint16_t i = 0; i < bottomBarLenght; i++)
-	        {
+            {
                 display.print('_');
             }
-           
-	    }
-        display.display();       
+        }
+        display.display();
     }
 
-
-    void showSettings(uint16_t minValue, uint16_t maxValue, const char* name, UF16x2 value)
+    void showSettings(uint16_t minValue, uint16_t maxValue, const char *name, UF16x2 value)
     {
         display.clearDisplay();
         display.setTextSize(1);
-	    display.println("USTAWIENIA");
-  	    display.println(name);
-		display.print("od "); display.print(minValue); display.print(" do "); display.println(maxValue);
+        display.println("USTAWIENIA");
+        display.println(name);
+        display.print("od ");
+        display.print(minValue);
+        display.print(" do ");
+        display.println(maxValue);
         display.println();
-		display.setTextSize(2);
-		display.println(value.asFloat());   
-        display.display(); 
+        display.setTextSize(2);
+        display.println(value.asFloat());
+        display.display();
     }
 
-    void showClickOption(const char* text, uint16_t fontSize, uint16_t pause)
+    void showClickOption(const char *text, uint16_t fontSize, uint16_t pause)
     {
         display.clearDisplay();
         display.setTextSize(fontSize);
-	    display.println(text);
-        display.display(); 
-        delay(pause);  
+        display.println(text);
+        display.display();
+        delay(pause);
     }
 
     void showSave()
     {
         display.clearDisplay();
         display.setTextSize(1);
-		display.println("Ustawienia");
-        display.println("zapisane");   
-        display.display();     
+        display.println("Ustawienia");
+        display.println("zapisane");
+        display.display();
     }
 
 }
