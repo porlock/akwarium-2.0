80c49d1e978a2ec8102c0d56ec038a35761b4b53
Tomasz Zielinski <Tomasz.Zielinski@vimn.com>
2021-11-08 20:30:43 +0100

    wartosci dla settingsGrades i settingsLimits

--- src/OptionsScreen.cpp
+++ src/OptionsScreen.cpp
@@ -4,6 +4,8 @@
 OptionsScreen::OptionsScreen(SettingsType& _settings) : m_settings(_settings)
 {
     displaySettingScreens=true;
+    settingsGrades={0.1f,0.5f,1,1};
+    settingsLimits={8,6,20,35,60,1,30,1};
 }
 
 void OptionsScreen::changeSetting(uint16_t screenState, bool add)
